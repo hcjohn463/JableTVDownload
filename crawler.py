cc4eb354258ac38651884359f105b3c3541f7bfe
hcjohn463 <hcjohn463@gmail.com>
2021-05-05 15:51:28 +0800

    Update crawler.py
    make full download

--- crawler.py
+++ crawler.py
@@ -3,11 +3,11 @@ from config import headers
 from functools import partial
 import concurrent.futures
 import time
+import copy
 
-def scrape(ci,folderPath,urls):
+def scrape(ci,folderPath,downloadList,urls):
     response = requests.get(urls, headers=headers, timeout=10)
     content_ts = response.content
-    
     
     fileName = urls.split('/')[-1][0:-3]
     with open(folderPath + "\\" + fileName + ".mp4", 'ab') as f:
@@ -15,16 +15,25 @@ def scrape(ci,folderPath,urls):
             f.write(ci.decrypt(content_ts))  # 解碼
         else:
             f.write(content_ts)
+        print('Download: ',urls.split('/')[-1])
+        downloadList.remove(urls)
         f.close()
 
-def startCrawl(ci,folderPath,tsList):
+def prepareCrawl(ci,folderPath,tsList):
+    downloadList = copy.deepcopy(tsList)
     #開始時間
     start_time = time.time()
-    print('正在下載 ' + str(len(tsList)) + ' 個檔案.. (預計等待時間: 10 ~ 15 分 視影片長度與網路速度而定)')
- 
-    #同時建立及啟用 50個執行緒
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-        executor.map(partial(scrape,ci,folderPath), tsList)
- 
+    print('開始下載 ' + str(len(downloadList)) + ' 個檔案..',end = '')
+    print('預計等待時間: {0:.2f} 分鐘 視影片長度與網路速度而定)'.format(len(downloadList) / 150))
+
+    #開始爬取
+    startCrawl(ci,folderPath,downloadList)
+
     end_time = time.time()
-    print(f"花費 {end_time - start_time} 秒爬取 {len(tsList)} 段 ts")
+    print('花費 {0:.2f} 分鐘 爬取完成 !'.format((end_time - start_time) / 60))
+
+def startCrawl(ci,folderPath,downloadList):
+    #同時建立及啟用 20個執行緒
+    while(downloadList != []):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            executor.map(partial(scrape,ci,folderPath,downloadList), downloadList)
