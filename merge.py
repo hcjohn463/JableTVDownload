849336dca8afc93bc4f2e38ccd4493a8da44060a
hcjohn463 <hcjohn463@gmail.com>
2021-05-20 03:05:06 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,20 +1,11 @@
 import time
 import os
 def mergeMp4(folderPath,tsList):
-
-	#開始時間
-	start_time = time.time()
-	print('開始合成影片..')
-
 	for i in range(len(tsList)):
 		file = tsList[i].split('/')[-1][0:-3] + '.mp4'
 		if os.path.exists(folderPath + '/' + file):
 			with open(folderPath + '/' + file, 'rb') as f1:
-				with open(folderPath + '/' + folderPath.split('/')[-1] + ".mp4", 'ab') as f2:
+				with open(folderPath + '/o' + folderPath.split('/')[-1] + '.mp4', 'ab') as f2:
 					f2.write(f1.read())
 		else:
 			print(file + " 失敗 ")
-
-	end_time = time.time()
-	print('花費 {0:.2f} 秒合成影片'.format(end_time - start_time))
-	print('下載完成!')
