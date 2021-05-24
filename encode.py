257b536787c458ea2103e93787e779194ce046c8
hcjohn463 <hcjohn463@gmail.com>
2021-05-24 22:51:27 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,27 +1,7 @@
-import ffmpeg
-import time
+import os
 def encodeMp4(folderPath):
-    #開始時間
-    start_time = time.time()
-    print('開始合成影片..')
-
-    originFile = folderPath + '/o' + folderPath.split('/')[-1] + '.mp4'
-    needFile = folderPath + '/' + folderPath.split('/')[-1] + '.mp4'
-    
-    output_args = {
-        "c:v" : "libx264",
-        "b:v" : "2M",
-        "threads" : 5,
-        "preset": "superfast",
-    }
-
-    (
-        ffmpeg
-        .input(originFile)
-        .output(needFile, **output_args)
-        .run()
-    )
-
-    end_time = time.time()
-    print('花費 {0:.2f} 秒合成影片'.format(end_time - start_time))
-    print('下載完成!')
+	number = folderPath.split('/')[-1]
+	before = number + '/o' + number + '.mp4'
+	after = number + '/' + number + '.mp4'
+	command = 'ffmpeg -i ' + before + ' -c:v libx264 -b:v 2M -threads 5 -preset superfast ' + after
+	os.system(command)
