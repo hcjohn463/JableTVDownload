f1934a9190a5bf89e54ba6a86da94d4bba832a7d
aki <aki@aki.local>
2021-05-28 02:23:02 +0800

    fix merge path

--- merge.py
+++ merge.py
@@ -1,12 +1,14 @@
-import time
 import os
 
-def mergeMp4(folderPath,tsList):
-	for i in range(len(tsList)):
-		file = tsList[i].split('/')[-1][0:-3] + '.mp4'
-		if os.path.exists(folderPath + '/' + file):
-			with open(folderPath + '/' + file, 'rb') as f1:
-				with open(folderPath + '/o' + folderPath.split('/')[-1] + '.mp4', 'ab') as f2:
-					f2.write(f1.read())
-		else:
-			print(file + " 失敗 ")
+
+def mergeMp4(folderPath, tsList):
+    for i in range(len(tsList)):
+        file = tsList[i].split('/')[-1][0:-3] + '.mp4'
+        full_path = os.path.join(folderPath, file)
+        video_name = folderPath.split(os.path.sep)[-1]
+        if os.path.exists(full_path):
+            with open(full_path, 'rb') as f1:
+                with open(os.path.join(folderPath, 'o' + video_name + '.mp4'), 'ab') as f2:
+                    f2.write(f1.read())
+        else:
+            print(file + " 失敗 ")
