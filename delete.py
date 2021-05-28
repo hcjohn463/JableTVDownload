e90346edefaa9a18ee75f37298fe852e85f89882
hcjohn463 <hcjohn463@gmail.com>
2021-05-28 11:22:58 +0800

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,15 +1,11 @@
 import os
 
-
-def deleteMp4(folderPath, flag):
+def deleteMp4(folderPath):
     files = os.listdir(folderPath)
-    originFile = 'o' + folderPath.split(os.path.sep)[-1] + '.mp4'
-    if flag:
-        for file in files:
-            if file != originFile:
-                os.remove(os.path.join(folderPath, file))
-    else:
-        os.remove(os.path.join(folderPath, originFile))
+    originFile = folderPath.split(os.path.sep)[-1] + '.mp4'
+    for file in files:
+        if file != originFile:
+            os.remove(os.path.join(folderPath, file))
 
 
 def deleteM3u8(folderPath):
