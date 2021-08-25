10e21daf72f0906e355918a03194417685ee2dc1
hcjohn463 <hcjohn463@gmail.com>
2021-08-25 22:05:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 headers = {
-    'referer':'https://jable.tv/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
-    'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-    'Accept-Encoding': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 }
