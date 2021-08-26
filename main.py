fb054627b16a45f606c68d63ff60b661c47fb5c8
JieHong-Liu <jiehong0914@gmail.com>
2021-08-26 22:54:50 +0800

    add args and random download function

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ from config import headers
 from crawler import prepareCrawl
 from merge import mergeMp4
 from delete import deleteM3u8, deleteMp4
-import time, cloudscraper
-
+import time
+import cloudscraper
+from args import *
 # In[2]:
 
+parser = get_parser()
+args = parser.parse_args()
 
-# Jable網址
-url = input('輸入jable網址:')
+if(len(args.url) != 0):
+    url = args.url
+elif(args.random == True):
+    url = av_recommand()
+else:
+    # 使用者輸入Jable網址
+    url = input('輸入jable網址:')
 
 # In[3]:
 
@@ -32,8 +40,6 @@ dirName = urlSplit[-2]
 if not os.path.exists(dirName):
     os.makedirs(dirName)
 folderPath = os.path.join(os.getcwd(), dirName)
-
-
 # In[4]:
 
 
