d5e36e94e256c86b0523a25768980c293ab222f1
張信中 <hcjohn463@gmail.com>
2021-08-29 23:01:52 +0800

    Revert "add args and random download function"
    This reverts commit fb054627b16a45f606c68d63ff60b661c47fb5c8.

--- main.py
+++ main.py
@@ -15,21 +15,13 @@ from config import headers
 from crawler import prepareCrawl
 from merge import mergeMp4
 from delete import deleteM3u8, deleteMp4
-import time
-import cloudscraper
-from args import *
+import time, cloudscraper
+
 # In[2]:
 
-parser = get_parser()
-args = parser.parse_args()
 
-if(len(args.url) != 0):
-    url = args.url
-elif(args.random == True):
-    url = av_recommand()
-else:
-    # 使用者輸入Jable網址
-    url = input('輸入jable網址:')
+# Jable網址
+url = input('輸入jable網址:')
 
 # In[3]:
 
@@ -40,6 +32,8 @@ dirName = urlSplit[-2]
 if not os.path.exists(dirName):
     os.makedirs(dirName)
 folderPath = os.path.join(os.getcwd(), dirName)
+
+
 # In[4]:
 
 
