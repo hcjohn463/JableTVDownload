e0128b06d434dc272b7de157101a5db415544903
hcjohn463 <hcjohn463@gmail.com>
2023-04-20 20:23:54 +0800

    edit encode.py

--- encode.py
+++ encode.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
-def ffmpeg_encode(folder_path, file_name,action):
+def ffmpeg_encode(folder_path, file_name, action):
     if action == 0: #不轉檔
         return
     elif action == 1: #GPU轉檔
+        os.chdir(folder_path)
         try:
             subprocess.call(['ffmpeg', '-i', f'{file_name}.mp4','-c:v', 'h264_nvenc', '-b:v', '10000K',
                                 '-threads', '5', f'f_{file_name}.mp4'])
@@ -14,6 +15,7 @@ def ffmpeg_encode(folder_path, file_name,action):
         except:
             print("轉檔失敗!")
     elif action == 2: #CPU轉檔
+        os.chdir(folder_path)
         try:
             subprocess.call(['ffmpeg', '-i', f'{file_name}.mp4', '-c:v', 'libx264', '-b:v', '3M',
                             '-threads', '5', '-preset', 'superfast', f'f_{file_name}.mp4'])
