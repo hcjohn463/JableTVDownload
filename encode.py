621dbc5bcb126fda8f45cb34a2aa9614584b0759
hcjohn463 <hcjohn463@gmail.com>
2023-04-20 11:12:36 +0800

    create encode action

--- encode.py
+++ encode.py
@@ -1,13 +1,22 @@
 import os
 import subprocess
 def ffmpeg_encode(folder_path, file_name):
-    os.chdir(folder_path)
-    try:
-        subprocess.call(['ffmpeg', '-i', f'{file_name}.mp4','-c:v', 'h264_nvenc', '-b:v', '10000K',
-                        '-threadss', '5', f'f_{file_name}.mp4'])
-        #os.remove(os.path.join(folder_path, f'{file_name}.mp4'))
-        #os.rename(os.path.join(folder_path, f'f_{file_name}.mp4'), os.path.join(folder_path, f'{file_name}.mp4'))
-        print("轉檔成功!")
+    action = input('要轉檔嗎?(y/n)')
+    if(action.lower() == "y"):
+        os.chdir(folder_path)
+        try:
+            action = input('要使用Nvidia硬體加速嗎?(y/n)')
+            if(action.lower() == "y"):
+                subprocess.call(['ffmpeg', '-i', f'{file_name}.mp4','-c:v', 'h264_nvenc', '-b:v', '10000K',
+                                '-threads', '5', f'f_{file_name}.mp4'])
+            else:
+                subprocess.call(['ffmpeg', '-i', f'{file_name}.mp4', '-c:v', 'libx264', '-b:v', '3M',
+                            '-threads', '5', '-preset', 'superfast', f'f_{file_name}.mp4'])
+            os.remove(os.path.join(folder_path, f'{file_name}.mp4'))
+            os.rename(os.path.join(folder_path, f'f_{file_name}.mp4'), os.path.join(folder_path, f'{file_name}.mp4'))
+            print("轉檔成功!")
 
-    except:
-        print("轉檔失敗!")
+        except:
+            print("轉檔失敗!")
+    else:
+        return
